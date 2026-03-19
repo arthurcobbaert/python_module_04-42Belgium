dbf995c7206023e151ca3c03e3bab966a1cb191f
Arthur Cobbaert <arthur.cobbaert@gmail.com>
2026-03-19 13:25:35 +0100

    flake8

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -1,14 +1,14 @@
 if __name__ == "__main__":
-	file_name = "new_discovery.txt"
-	print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n")
-	with open(file_name, "w") as f:
-		print(f"Initializing new storage unit: {file_name}")
-		print("Storage unit created successfully...\n")
-		print("Inscribing preservation data...")
-		f.write("[ENTRY 001] New quantum algorithm discovered\n")
-		f.write("[ENTRY 002] Efficiency increased by 347%\n")
-		f.write("[ENTRY 003] Archived by Data Archivist trainee\n")
-	with open(file_name, "r") as r:
-		print(r.read())
-		print("Data inscription complete. Storage unit sealed.")
-		print("Archive 'new_discovery.txt' ready for long-term preservation.")
+    file_name = "new_discovery.txt"
+    print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n")
+    with open(file_name, "w") as f:
+        print(f"Initializing new storage unit: {file_name}")
+        print("Storage unit created successfully...\n")
+        print("Inscribing preservation data...")
+        f.write("[ENTRY 001] New quantum algorithm discovered\n")
+        f.write("[ENTRY 002] Efficiency increased by 347%\n")
+        f.write("[ENTRY 003] Archived by Data Archivist trainee\n")
+    with open(file_name, "r") as r:
+        print(r.read())
+        print("Data inscription complete. Storage unit sealed.")
+        print("Archive 'new_discovery.txt' ready for long-term preservation.")
--- ex2/ft_stream_management.py
+++ ex2/ft_stream_management.py
@@ -1,12 +1,21 @@
 import sys
 
+
 def stream_management() -> None:
-	print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
-	arch_id = input("Input stream active. Enter archivist ID: ")
-	report = input("Input stream active. Enter status report: ")
-	print(f"\n[STANDARD] Archive status from ARCH_{arch_id}: {report}", file=sys.stdout)
-	print("[ALERT] System diagnostic: Communication channels verified", file=sys.stderr)
-	print("[STANDARD] Data transmission complete", file=sys.stdout)
-	print("\nThree-channel communication test successful.")
+    print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
+    arch_id = input("Input stream active. Enter archivist ID: ")
+    report = input("Input stream active. Enter status report: ")
+    print(
+        f"\n[STANDARD] Archive status from ARCH_{arch_id}: {report}",
+        file=sys.stdout
+    )
+    print(
+        "[ALERT] System diagnostic: Communication channels verified",
+        file=sys.stderr
+    )
+    print("[STANDARD] Data transmission complete", file=sys.stdout)
+    print("\nThree-channel communication test successful.")
+
+
 if __name__ == "__main__":
-	stream_management()
+    stream_management()
